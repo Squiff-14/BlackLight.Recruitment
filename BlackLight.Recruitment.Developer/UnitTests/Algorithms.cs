7ad1399786b1c68f287430052000e475e6825d1d
Ben <BenAsquith@netconstruct.co.uk>
2022-07-24 10:01:28 +0100

    adding project files to git

---
+++ BlackLight.Recruitment.Developer/UnitTests/Algorithms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackLight.Recruitment.UnitTests
+{
+    class Algorithms
+    {
+        public static string ReverseWordsInString(string input)
+        {
+            return input;
+        }
+
+        public static string ReverseWordsInStringWithoutUsingAnyLinqMethods(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int[] SortWithoutUsingBuiltInSortMethods(int[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
